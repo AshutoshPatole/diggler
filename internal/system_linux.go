1a3743c754e74824442b80d07af1cceaf6eb4810
AshutoshPatole <apatole479@gmail.com>
2025-09-05 16:06:37 +0530

    🪶  Use OS specific files for selinux and appaprmor

--- internal/system_linux.go
+++ internal/system_linux.go
@@ -1,31 +1,37 @@
+//go:build linux
 // +build linux
 
 package internal
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/moby/docker/profiles/apparmor"
-	"github.com/opencontainers/selinux/go-selinux"
 )
 
 func GetSELinuxInfo() {
-	if selinux.GetEnabled() {
-		t := table.NewWriter()
-		t.SetOutputMirror(os.Stdout)
-		t.SetTitle("SELinux Information")
-		t.AppendHeader(table.Row{"Category", "Value"})
-		t.AppendRows([]table.Row{
-			{"SELinux Status", "Enabled"},
-			{"Enforce Mode", selinux.EnforceMode()},
-		})
-		t.Render()
+	selinuxStatus, err := os.ReadFile("/sys/fs/selinux/enforce")
+	if err != nil {
+		return
 	}
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.SetTitle("SELinux Information")
+	t.AppendHeader(table.Row{"Category", "Value"})
+	t.AppendRows([]table.Row{
+		{"SELinux Status", string(selinuxStatus)},
+		{"Enforce Mode", filepath.Base(string(selinuxStatus))},
+	})
+	t.Render()
 }
 
 func GetAppArmorInfo() {
-	if apparmor.IsLoaded("") {
+	profile, err := os.ReadFile("/proc/self/attr/current")
+	if err != nil {
+		return
+	}
+	if string(profile) != "unconfined" {
 		t := table.NewWriter()
 		t.SetOutputMirror(os.Stdout)
 		t.SetTitle("AppArmor Information")
