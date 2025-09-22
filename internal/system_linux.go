50243597ddfa3c14a8781101842a0228c8c5eb73
AshutoshPatole <apatole479@gmail.com>
2025-09-22 14:33:00 +0530

    🛡️  Add firewal status

--- internal/system_linux.go
+++ internal/system_linux.go
@@ -21,27 +21,52 @@ func isCommandAvailable(command string) bool {
 	return err == nil
 }
 
-func getSELinuxInfo() {
-	selinuxStatus, err := runCommand("sestatus")
-	if err != nil {
-		return
-	}
-	fmt.Printf("\n\nSELinux Information:\n%s\n", selinuxStatus)
-}
-
-func getAppArmorInfo() {
-	profile, err := runCommand("apparmor_status")
-	if err != nil {
-		return
-	}
-	fmt.Printf("\n\nAppArmor Information:\n%s\n", profile)
-}
-
 func GetSecurityInfo() {
 	if isCommandAvailable("sestatus") {
-		getSELinuxInfo()
+		selinuxStatus, err := runCommand("sestatus")
+		if err != nil {
+			return
+		}
+		fmt.Printf("\n\nSELinux Information:\n%s\n", selinuxStatus)
 	}
 	if isCommandAvailable("apparmor_status") {
-		getAppArmorInfo()
+		profile, err := runCommand("apparmor_status")
+		if err != nil {
+			return
+		}
+		fmt.Printf("\n\nAppArmor Information:\n%s\n", profile)
+	}
+}
+
+func FirewallStat() {
+	if isCommandAvailable("firewall-cmd") {
+		fmt.Printf("\n\nFirewall Status")
+		status, err := runCommand("firewall-cmd", "--state")
+		if err != nil {
+			return
+		}
+		fmt.Printf("Status: %s\n", status)
+
+		rules, err := runCommand("firewall-cmd", "--list-all")
+		if err != nil {
+			return
+		}
+		fmt.Printf("Rules: %s\n", rules)
+	}
+
+	if isCommandAvailable("ufw") {
+
+		fmt.Printf("\n\nUFW Status")
+		status, err := runCommand("ufw", "status")
+		if err != nil {
+			return
+		}
+		fmt.Printf("Status: %s\n", status)
+
+		rules, err := runCommand("ufw", "status", "verbose")
+		if err != nil {
+			return
+		}
+		fmt.Printf("Rules: %s\n", rules)
 	}
 }
